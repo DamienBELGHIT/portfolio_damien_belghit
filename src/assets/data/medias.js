98a1d1c1956d43e3ae7cfba4ef0ab30dcc001173
Damien BELGHIT <46059249+DamienBELGHIT@users.noreply.github.com>
2024-11-21 15:05:07 +0100

    Update medias.js

--- src/assets/data/medias.js
+++ src/assets/data/medias.js
@@ -3,46 +3,59 @@ const medias = [
     type: "Sites",
     medias: [
       {
+        title: "Cauldron Caution",
+        description: "Site de mon jeu de cartes et d'aventure",
+        img: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2991890/header.jpg?t=1730569063",
+        link: "https://cauldron-caution.com/",
+      },
+      {
+        title: "La Crapouillerie",
+        description:
+          "Site officiel d'une association d'éducation populaire en React",
+        img: "https://i.postimg.cc/0NKsYW4R/Crapouilles.png",
+        link: "https://lacrapouillerie.fr",
+      },
+      {
         title: "Portfolio",
         description: "Mon site portfolio en React",
-        img: "/src/assets/img/SitePortfolio.PNG",
+        img: "https://i.postimg.cc/65CpwQgz/Site-Portfolio.png",
         link: "https://github.com/DamienBELGHIT/portfolio_damien_belghit",
       },
       {
         title: "Les Petits Plats",
         description: "Site de recherche de recettes en JS pur",
-        img: "/src/assets/img/SitePetitsPlats.PNG",
+        img: "https://i.postimg.cc/zDbft7ks/Site-Petits-Plats.png",
         link: "https://damienbelghit.github.io/LesPetitsPlats-Projet5/",
       },
       {
         title: "SportSee",
         description: "Tableau analytics sportif en React + Recharts",
-        img: "/src/assets/img/SiteSportSee.PNG",
+        img: "https://i.postimg.cc/CxYLKgV0/Site-Sport-See.png",
         link: "https://github.com/DamienBELGHIT/SportSee",
       },
       {
         title: "Fisheye",
         description: "Site de photographes axé accessibilité",
-        img: "/src/assets/img/SiteFisheye.PNG",
+        img: "https://i.postimg.cc/Y0mT6hYP/Site-Fisheye.png",
         link: "https://github.com/DamienBELGHIT/Fisheye",
       },
       {
         title: "Kasa",
         description: "Site AirBnB en React",
-        img: "/src/assets/img/SiteKasa.PNG",
+        img: "https://i.postimg.cc/kD3zfqyg/SiteKasa.png",
         link: "https://damienbelghit.github.io/kasa",
       },
       {
         title: "DataTable",
         description:
           "Librairie React permettant d'insérer des tableaux de données intéractifs",
-        img: "/src/assets/img/SiteDataTable.PNG",
+        img: "https://i.postimg.cc/CKhy3MpZ/Site-Data-Table.png",
         link: "https://www.npmjs.com/package/react-interactive-datatable?activeTab=explore",
       },
       {
         title: "ohmyfood",
         description: "Site de menu de restauration en Sass",
-        img: "/src/assets/img/SiteOhMyFood.PNG",
+        img: "https://i.postimg.cc/RCkZjtsN/Site-Oh-My-Food.png",
         link: "https://damienbelghit.github.io/ohmyfood/",
       },
     ],
@@ -51,10 +64,22 @@ const medias = [
     type: "Jeux",
     medias: [
       {
+        title: "Cauldron Caution",
+        description: "Jeu de cartes et d'aventure",
+        img: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2991890/header.jpg?t=1730569063",
+        link: "https://store.steampowered.com/app/2991890/Cauldron_Caution/",
+      },
+      {
         title: "Hex & Tricks",
         description: "Jeu de cartes et d'aventure",
         img: "https://cdn.akamai.steamstatic.com/steam/apps/2098550/capsule_616x353.jpg?t=1675762142",
         link: "https://store.steampowered.com/app/2098550/Hex_And_Tricks/",
+      },
+      {
+        title: "Haircut Express",
+        description: "Jeu réalisé en 3 jours pour la Mini Jam Game #18",
+        img: "https://img.itch.zone/aW1nLzEyMzc0NjM4LnBuZw==/315x250%23c/%2BpfYOJ.png",
+        link: "https://poishiche.itch.io/haircut-express",
       },
       {
         title: "Wild Fire",
@@ -92,33 +117,39 @@ const medias = [
     type: "Illustrations",
     medias: [
       {
-        title: "Vectoriel",
-        img: "/src/assets/img/VectorialThumbnail.PNG",
+        title: "Painting",
+        img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/29916100-a792-456b-83aa-88dfd69958a7/dgreoga-2e0dadc1-7e31-4173-bd97-c50a10b2b75a.png/v1/fill/w_941,h_768,q_80,strp/esteban_the_pirate__ellsmart_oc__by_poisheesh_dgreoga-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzY4IiwicGF0aCI6IlwvZlwvMjk5MTYxMDAtYTc5Mi00NTZiLTgzYWEtODhkZmQ2OTk1OGE3XC9kZ3Jlb2dhLTJlMGRhZGMxLTdlMzEtNDE3My1iZDk3LWM1MGExMGIyYjc1YS5wbmciLCJ3aWR0aCI6Ijw9OTQxIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.5POnMT__Vbq-0ECUhexvAEOczGEd_RAT8NNk6l-u_rQ",
         list: [
-          "/src/assets/img/Sui.png",
-          "/src/assets/img/Sloth.jpg",
-          "/src/assets/img/Chibi.png",
-          "/src/assets/img/TeteDessin.png",
-          "/src/assets/img/Reinald.png",
+          "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/29916100-a792-456b-83aa-88dfd69958a7/dgreoga-2e0dadc1-7e31-4173-bd97-c50a10b2b75a.png/v1/fill/w_941,h_768,q_80,strp/esteban_the_pirate__ellsmart_oc__by_poisheesh_dgreoga-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzY4IiwicGF0aCI6IlwvZlwvMjk5MTYxMDAtYTc5Mi00NTZiLTgzYWEtODhkZmQ2OTk1OGE3XC9kZ3Jlb2dhLTJlMGRhZGMxLTdlMzEtNDE3My1iZDk3LWM1MGExMGIyYjc1YS5wbmciLCJ3aWR0aCI6Ijw9OTQxIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.5POnMT__Vbq-0ECUhexvAEOczGEd_RAT8NNk6l-u_rQ",
+          "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/29916100-a792-456b-83aa-88dfd69958a7/dgq644w-f9167f44-8aed-4832-817e-345d0e890cb9.png/v1/fit/w_828,h_1184,q_70,strp/lysandre_the_malicious_by_poisheesh_dgq644w-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTgyOSIsInBhdGgiOiJcL2ZcLzI5OTE2MTAwLWE3OTItNDU2Yi04M2FhLTg4ZGZkNjk5NThhN1wvZGdxNjQ0dy1mOTE2N2Y0NC04YWVkLTQ4MzItODE3ZS0zNDVkMGU4OTBjYjkucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.7QbciFDvnAthsaOW2okiarejLPRD6tROa0F8AA_GBOI",
+          "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/29916100-a792-456b-83aa-88dfd69958a7/dgsg9ut-9ca3a4a1-fe25-4b69-ae21-00d979f8c2b8.png/v1/fill/w_979,h_476,q_80,strp/noah_and_maxwell_by_poisheesh_dgsg9ut-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NDc2IiwicGF0aCI6IlwvZlwvMjk5MTYxMDAtYTc5Mi00NTZiLTgzYWEtODhkZmQ2OTk1OGE3XC9kZ3NnOXV0LTljYTNhNGExLWZlMjUtNGI2OS1hZTIxLTAwZDk3OWY4YzJiOC5wbmciLCJ3aWR0aCI6Ijw9OTc5In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.jP6sEjtuXYYLf1MT1YhjzUm_JWjc5bEVU_hxYHp_Un8",
         ],
       },
       {
-        title: "Tablette",
-        img: "/src/assets/img/CutHair.PNG",
-        list: ["/src/assets/img/CutHair.PNG", "/src/assets/img/Holga.PNG"],
+        title: "Line Art",
+        img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/29916100-a792-456b-83aa-88dfd69958a7/did5bwh-b6b6f52c-8080-4f3e-a48e-9ccabbd2aef0.png/v1/fit/w_828,h_1030,q_70,strp/yeast_fight_by_poisheesh_did5bwh-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU5MSIsInBhdGgiOiJcL2ZcLzI5OTE2MTAwLWE3OTItNDU2Yi04M2FhLTg4ZGZkNjk5NThhN1wvZGlkNWJ3aC1iNmI2ZjUyYy04MDgwLTRmM2UtYTQ4ZS05Y2NhYmJkMmFlZjAucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.USdPqgYyiuqSbmfscmwE5A30JkUf64dM84jG_o4BYyg",
+        list: [
+          "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/29916100-a792-456b-83aa-88dfd69958a7/did5bwh-b6b6f52c-8080-4f3e-a48e-9ccabbd2aef0.png/v1/fit/w_828,h_1030,q_70,strp/yeast_fight_by_poisheesh_did5bwh-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU5MSIsInBhdGgiOiJcL2ZcLzI5OTE2MTAwLWE3OTItNDU2Yi04M2FhLTg4ZGZkNjk5NThhN1wvZGlkNWJ3aC1iNmI2ZjUyYy04MDgwLTRmM2UtYTQ4ZS05Y2NhYmJkMmFlZjAucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.USdPqgYyiuqSbmfscmwE5A30JkUf64dM84jG_o4BYyg",
+          "https://t.ly/XMIJw",
+          "https://i.postimg.cc/kgF3Y6z3/Chibi.png",
+          "https://t.ly/6eu06",
+        ],
       },
       {
         title: "3D",
-        img: "/src/assets/img/Bottaro.png",
-        list: ["/src/assets/img/Bottaro.png", "/src/assets/img/Donut.PNG"],
+        img: "https://i.postimg.cc/8cJgZyQt/Bottaro.png",
+        list: [
+          "https://i.postimg.cc/8cJgZyQt/Bottaro.png",
+          "https://i.postimg.cc/sDWRS0J2/Donut.png",
+        ],
       },
       {
         title: "Pixel Art",
-        img: "/src/assets/img/PixelArtThumbnail.PNG",
+        img: "https://i.postimg.cc/JhpwMwRp/Race.png",
         list: [
-          "/src/assets/img/Race.png",
-          "/src/assets/img/Dizzy.png",
-          "/src/assets/img/Artificial.png",
+          "https://i.postimg.cc/JhpwMwRp/Race.png",
+          "https://i.postimg.cc/tgpy9tW4/Artificial.png",
+          "https://i.postimg.cc/fyGnpxMZ/Dizzy.png",
         ],
       },
     ],
